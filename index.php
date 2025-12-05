794e6c9a3bcde21c02b47e5452d5868cd77273b0
ishiwatomo <tomo.ishiwa@neo-career.co.jp>
2025-12-05 18:28:54 +0900

    Merit PC途中

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>基本のHTMLテンプレート</title>
   <link rel="stylesheet" href="style.css"/>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 </head>
 <body>
 <header>
@@ -56,8 +57,175 @@
     </div>
   </section>
 
-  <section id="">
+  <section id="merit">
+    <div class="merit_wrapper">
+      <div class="section_title">
+        <h2>MERIT</h2>
+        <h1>パートナー契約を結ぶ<br>
+          <span>3</span>つのメリット</h1>
+      </div>
+      <div class="merit_content">
+        <div class="merit_block">
+          <div class="text_block">
+            <div class="num">
+              <span>MERIT</span>
+              <span class="number">01</span>
+            </div>
+            <div class="sub_title">
+              <p>圧倒的な取り扱い商材</p>
+            </div>
+            <p class="text">総合求人広告代理店であるネオキャリアでは、中途、新卒、アルバイト、幅広い雇用形態で充実したサービス数をそろえています。<br>
+              <span>メインとなる求人広告のほとんどを好条件でお取り扱いいただけます。</span></p>
+          </div>
+          <div class="img_block">
+            <img src="assets/images/merit1.png" alt="merit1">
+          </div>
+        </div>
 
+        <div class="merit_block">
+          <div class="text_block">
+            <div class="num">
+              <span>MERIT</span>
+              <span class="number">02</span>
+            </div>
+            <div class="sub_title">
+              <p>手厚いサポート体制</p>
+            </div>
+            <p class="text"><span>専任の渉外営業担当がつき、パートナー様の営業活動やサポート活動などを一緒にさせていただきます。</span><br>
+              いつでもご連絡いただけるよう、専用のチャットツールとパートナーシステムを導入しています。いつでも最新の情報が確認でき、お気軽にご相談していただける体制となっていますので、安心して営業活動をおこなっていただけます。</p>
+          </div>
+          <div class="img_block">
+            <img src="assets/images/merit2.png" alt="merit2">
+          </div>
+        </div>
+
+        <div class="merit_block">
+          <div class="text_block">
+            <div class="num">
+              <span>MERIT</span>
+              <span class="number">03</span>
+            </div>
+            <div class="sub_title">
+              <p>各求人メディアとの強いつながり</p>
+            </div>
+            <p class="text">ネオキャリアでは、エン転職やマイナビ新卒など、国内大手求人メディアにおいて<span>豊富な販売実績があるため、良い条件・ロイヤリティで販売ができます。</span>そのためパートナー企業の皆様にも好条件で販売していただくことができ、メーカー実施の勉強会やキャンペーンなどにもご参加いただけます。</p>
+          </div>
+          <div class="img_block">
+            <img src="assets/images/merit3.png" alt="merit3">
+          </div>
+        </div>
+
+        <div class="merit_block">
+          <div class="text_block">
+            <div class="num">
+              <span>MERIT</span>
+              <span class="number">04</span>
+            </div>
+            <div class="sub_title">
+              <p>様々な角度の勉強会やイベントを実施</p>
+            </div>
+            <p class="text">・全体向けの勉強会<span>月6回ほど実施</span><br>
+              ・個社別の課題に合わせた<span>勉強会も実施</span><br>
+              ・テレアポ大会の実施や、年間表彰式の開催など<br>
+              ・ネオキャリア限定キャンペーンの実施</p>
+          </div>
+          <div class="img_block">
+            <img src="assets/images/merit4.png" alt="merit4">
+          </div>
+        </div>
+
+        <div class="merit_block">
+          <div class="text_block">
+            <div class="num">
+              <span>MERIT</span>
+              <span class="number">05</span>
+            </div>
+            <div class="sub_title">
+              <p class="text">求人広告以外のニーズにも対応</p>
+            </div>
+            <p>・企業立ち上げ時のリソース不足に合わせた<span>リーガルツールのご提供</span><br>
+              ・制作のサポート<br>
+              ・ATSやHPなどの制作物、RPO、オウントメディアなど</p>
+          </div>
+          <div class="img_block">
+            <img src="assets/images/merit5.png" alt="merit5">
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <section id="advertisement">
+    <div class="advertisement_wrapper">
+      <div class="section_title">
+        <h2>advertisement</h2>
+        <h1>取扱求人広告</h1>
+      </div>
+      <div class="advertisement_content">
+
+      </div>
+    </div>
+  </section>
+
+  <section id="member">
+    <div class="member_wrapper">
+      <div class="section_title">
+        <h2>MEMBER</h2>
+        <h1>メンバー紹介</h1>
+      </div>
+      <div class="member_content">
+
+      </div>
+    </div>
+  </section>
+
+  <section id="voice">
+    <div class="voice_wrapper">
+      <div class="section_title">
+        <h2>VOICE</h2>
+        <h1>パートナー様からの声</h1>
+      </div>
+      <div class="voice_content">
+
+      </div>
+    </div>
+  </section>
+
+  <section id="youtube">
+    <div class="youtube_wrapper">
+      <div class="section_title">
+        <h2>YOUTUBE</h2>
+        <h1>仕事のリアルを紹介</h1>
+      </div>
+      <div class="youtube_content">
+
+      </div>
+    </div>
+  </section>
+
+  <section id="qa">
+    <div class="qa_wrapper">
+      <div class="section_title">
+        <h2>Q&A</h2>
+        <h1>よくある質問</h1>
+      </div>
+      <div class="qa_content">
+
+      </div>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="contact_wrapper">
+      <div class="section_title">
+        <h2>contact</h2>
+        <h1>お問い合わせ</h1>
+      </div>
+      <div class="contact_content">
+
+      </div>
+    </div>
   </section>
 </main>
 
