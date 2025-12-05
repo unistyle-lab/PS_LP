413d6aa7eac6992fcab6c803cb035d611788bf35
ishiwatomo <tomo.ishiwa@neo-career.co.jp>
2025-12-05 17:31:32 +0900

    fv PC

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>基本のHTMLテンプレート</title>
+  <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<header>
+  <div class="header_wrapper">
+    <a class="logo">
+      <img src="assets/images/logo.png" alt="ロゴ画像">
+    </a>
+  </div>
+</header>
+
+<main>
+  <div class="header_padding"></div>
+  <section id="fv">
+    <div class="fv_bg">
+      <img src="assets/images/fv_bg.png" alt="背景画像">
+    </div>
+    <div class="fv_wrapper">
+      <div class="fv_title">
+        <img class="absolute" src="assets/images/fv_caption.png" alt="求人広告代理店パートナー募集">
+        <img src="assets/images/fv_title.png" alt="圧倒的なサービスラインナップとバックアップ">
+      </div>
+      <div class="fv_info">
+        <div class="frame_contents">
+          <img src="assets/images/fv_frame1.png" alt="営業研修ツール無料で利用可能">
+          <img src="assets/images/fv_frame2.png" alt="国内複数拠点で全国対応">
+          <img src="assets/images/fv_frame3.png" alt="二人三脚のサポート体制">
+        </div>
+        <div class="map_block">
+          <img class="map" src="assets/images/fv_map.png" alt="エリア展開画像">
+          <img class="absolute" src="assets/images/fv_person.png" alt="fv_person">
+        </div>
+      </div>
+      <div class="fv_contact">
+        <div class="orange_button">
+          <span>お問い合わせはこちら</span>
+          <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
+            <ellipse cx="16.9848" cy="16.9944" rx="16.9848" ry="16.9944" fill="white"/>
+            <path d="M24.7891 16.5351L12.3948 23.695L12.3948 9.37522L24.7891 16.5351Z" fill="url(#paint0_linear_3_170)"/>
+            <defs>
+              <linearGradient id="paint0_linear_3_170" x1="16.5262" y1="8.26758" x2="16.5262" y2="24.8026" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#FD8D00"/>
+                <stop offset="0.445" stop-color="#FC9D01"/>
+                <stop offset="1" stop-color="#FBA801"/>
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="">
+
+  </section>
+</main>
+
+<footer>
+  <p>© 2025 My Website</p>
+</footer>
+</body>
+</html>
